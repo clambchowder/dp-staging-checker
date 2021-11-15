88f5438263f55dbbcb550ee13d8457a182d5b2a2
Kyle Mitofsky <kmitofsky@dealerpolicy.com>
2021-11-15 08:27:41 -0500

    add retry logic

--- src/services/app-versions/app-versions-api.ts
+++ src/services/app-versions/app-versions-api.ts
@@ -1,8 +1,30 @@
 
 import { IApplicationInfoRow } from '../../models';
 
+type IRetryMethod = <T>(method: () => Promise<T>, maxAttempts: number) => Promise<T>
+
+const retryMethod: IRetryMethod = async (method, maxAttempts) => {
+    let curAttempt = 1
+    let err
+    while (curAttempt < maxAttempts) {
+        try {
+            const output = await method()
+            return output
+        } catch (error) {
+            curAttempt++
+            err = error
+        }
+    }
+    throw err
+};
+
 export const getAppVersions = async (): Promise<IApplicationInfoRow[]> => {
-    const resp = await fetch('/api/versions')
-    const data = await resp.json()
+    const fetchData = async () => {
+        const resp = await fetch('/api/versions')
+        const data = await resp.json()
+        return data
+    }
+    const data = await retryMethod(fetchData, 3)
+
     return data as IApplicationInfoRow[]
 }
