3a91002e50338472e4ea8497f926985349f5f85c
Tylor Mayfield <tmayfield@dealerpolicy.com>
2021-12-16 21:35:59 -0500

    Update versions.ts

--- src/functions/versions.ts
+++ src/functions/versions.ts
@@ -2,11 +2,9 @@ import { Handler } from "@netlify/functions";
 import { getAppVersions } from "../services/app-versions/app-versions-fetch";
 
 const handler: Handler = async (_event, _context) => {
-  let response;
-  try {
-    let results = await getAppVersions();
-    response = JSON.stringify(results, null, 2);
-  } catch (error) {}
+  let results = await getAppVersions();
+  let response = JSON.stringify([], null, 2);
+  if (results) response = JSON.stringify(results, null, 2);
 
   return {
     statusCode: 200,
