a0bd17d6eeb3a25c6605c0089fd903b176ef5cb9
clambchowder <102677659+clambchowder@users.noreply.github.com>
2022-03-31 15:48:59 -0400

    Change build behavior for default / prod builds (#1)
    * Change build behavior for default / prod builds
    
    Co-Authored-By: clambchowder <102677659+clambchowder@users.noreply.github.com>
    
    * Whitespace changes for build error
    
    Co-Authored-By: clambchowder <102677659+clambchowder@users.noreply.github.com>
    
    * More whitespace fixes for grumpy bot
    
    Co-Authored-By: clambchowder <102677659+clambchowder@users.noreply.github.com>

--- src/services/app-versions/index.ts
+++ src/services/app-versions/index.ts
@@ -1,13 +1,9 @@
 import * as AppVersionFetch from './app-versions-fetch'
 import * as AppVersionMock from './app-versions-mock'
-import * as AppVersionApi from './app-versions-api'
 
 const getAppVersionInstance = () => {
     if (Boolean(process.env.REACT_APP_USE_MOCK_DATA)) return AppVersionMock
-    if (Boolean(process.env.REACT_APP_USE_CLIENT_FETCH)) return AppVersionFetch
-    return AppVersionApi
-
+    return AppVersionFetch
 }
 
 export const { getAppVersions } = getAppVersionInstance()
-
