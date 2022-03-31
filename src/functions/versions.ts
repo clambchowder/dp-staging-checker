7f4563890998b70dc760dd725636dffc108f5e94
Tylor Mayfield <tmayfield@dealerpolicy.com>
2022-03-31 12:26:44 -0400

    try catch

--- src/functions/versions.ts
+++ src/functions/versions.ts
@@ -2,14 +2,18 @@ import { Handler } from "@netlify/functions";
 import { getAppVersions } from "../services/app-versions/app-versions-fetch";
 
 const handler: Handler = async (_event, _context) => {
-  let results = await getAppVersions();
-  let response = JSON.stringify([], null, 2);
-  if (results) response = JSON.stringify(results, null, 2);
+  try {
+    let results = await getAppVersions();
+    let response = JSON.stringify([], null, 2);
+    if (results) response = JSON.stringify(results, null, 2);
 
-  return {
-    statusCode: 200,
-    body: response,
-  };
+    return {
+      statusCode: 200,
+      body: response,
+    };
+  } catch (err) {
+    return { statusCode: 999, body: err };
+  }
 };
 
 export { handler };
